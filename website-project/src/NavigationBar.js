d0679ad72d9462994bd99224688b14fbcce31373
Facundo Marin <facundo.marin2012@gmail.com>
2022-01-17 11:30:52 -0300

    style the navigation bar, breakpoints

--- website-project/src/NavigationBar.js
+++ website-project/src/NavigationBar.js
@@ -1,15 +1,38 @@
 import { Box, Container, Link, Toolbar, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import React from "react";
 
+const useStyles = makeStyles((theme) => ({
+  menuBox: {
+    display: "flex",
+    flexDirection: "column",
+    [theme.breakpoints.up("md")]: {
+      flexDirection: "row",
+    },
+  },
+  menuOption: {
+    padding: theme.spacing(1),
+    [theme.breakpoints.up("md")]: {
+      paddingLeft: theme.spacing(3),
+    },
+  },
+}));
+
 export default function NavigationBar() {
+  const classes = useStyles();
+
   return (
     <Container>
       <Toolbar>
         <Typography>Mammoth Interactive</Typography>
 
-        <Box>
+        <Box className={classes.menuBox}>
           {["home", "courses", "sign up"].map((menuOption) => (
-            <Link component="button" variant="body1">
+            <Link
+              component="button"
+              variant="body1"
+              className={classes.menuOption}
+            >
               {menuOption.toUpperCase()}
             </Link>
           ))}
