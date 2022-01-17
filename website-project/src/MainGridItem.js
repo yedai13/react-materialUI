8df98205eccc7da380a6cf791f5b183e92ce70f3
Facundo Marin <facundo.marin2012@gmail.com>
2022-01-17 13:59:41 -0300

    style typography in main grid item

--- website-project/src/MainGridItem.js
+++ website-project/src/MainGridItem.js
@@ -9,16 +9,25 @@ const useStyles = makeStyles((theme) => ({
     color: theme.palette.common.white,
     backgroundSize: "cover",
   },
+  mainItem: {
+    padding: theme.spacing(6),
+    textAlign: "left",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+  },
 }));
 
 const MainGridItem = () => {
   const classes = useStyles();
   return (
-    <Grid>
-      <Grid item className={classes.mainContainer}>
-        <Typography>Hello Coding</Typography>
+    <Grid container className={classes.mainContainer}>
+      <Grid item className={classes.mainItem} md={6}>
+        <Typography component="h2" variant="h4">
+          Hello Coding
+        </Typography>
 
-        <Typography>
+        <Typography variant="body" paragraph>
           Do you want yo learn to code? Smash your career goals. Build websites,
           games, apps, and aritifial intelligence project in 99 days.
         </Typography>
