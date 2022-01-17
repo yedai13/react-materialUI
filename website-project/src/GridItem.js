229f01934c70143b4cbf63270eb663c98d17ff08
Facundo Marin <facundo.marin2012@gmail.com>
2022-01-17 14:58:42 -0300

    reuse a grid item card

--- website-project/src/GridItem.js
+++ website-project/src/GridItem.js
@@ -17,24 +17,20 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const GridItem = () => {
+const GridItem = (props) => {
   const classes = useStyles();
   return (
     <Card>
-      <CardHeader title="Excel Masterclass" subheader="Recommended for you" />
+      <CardHeader title={props.title} subheader="Recommended for you" />
 
       <CardMedia
         className={classes.cardImage}
-        image="https://cdn.pixabay.com/photo/2015/09/05/22/46/business-925900_1280.jpg"
+        image={props.image}
         title="Excel Masterclass"
       />
 
       <CardContent>
-        <Typography>
-          In this 6-level eDegree, you'll learn how to use Excel from the very
-          basics all the way to VBA programming and machine learning. You'll
-          also learn web automation, Pythom and Selenium
-        </Typography>
+        <Typography>{props.description}</Typography>
       </CardContent>
 
       <CardActions>
--- website-project/src/GridLayout.js
+++ website-project/src/GridLayout.js
@@ -28,25 +28,53 @@ const GridLayout = () => {
       </Grid>
 
       <Grid item sm={6} xs={12}>
-        <GridItem />
+        <GridItem
+          title="Excel Masterclass"
+          image="https://cdn.pixabay.com/photo/2015/09/05/22/46/business-925900_1280.jpg"
+          description="  In this 6-level eDegree, you'll learn how to use Excel from the very
+          basics all the way to VBA programming and machine learning. You'll
+          also learn web automation, Pythom and Selenium"
+        />
       </Grid>
 
       <Grid item sm={6} xs={12}>
-        <Typography className={classes.secondRowSecondItem}>
-          Second Row, Second Item
-        </Typography>
+        <GridItem
+          title="Learn to Code and Get Hired"
+          image="https://cdn.pixabay.com/photo/2021/01/21/14/44/laptop-5937580_1280.jpg"
+          description="Do you need a better career? Do you want to become an employed
+           software developer with a high paying job? Learn how to code and get hired in 
+           this coding interview questions course"
+        />
       </Grid>
 
       <Grid item sm={4} xs={12}>
-        <Typography className={classes.thirdRow}>Third Row</Typography>
+        <GridItem
+          title="3D Modeling Masterclass"
+          image="https://cdn.pixabay.com/photo/2020/01/20/19/08/modelling-4781127_1280.jpg"
+          description="In this 17-course bundle, you will build low poly and high poly 3D 
+          models. Anyone can learn the free digital art software Blender. You will also learn 
+          Unity, and Adobe Photoshop"
+        />
       </Grid>
 
       <Grid item sm={4} xs={12}>
-        <Typography className={classes.thirdRow}>Third Row</Typography>
+        <GridItem
+          title="Business & Side Hustles"
+          image="https://cdn.pixabay.com/photo/2020/12/31/17/39/man-5877068_1280.jpg"
+          description="This five-level course will take you from no experience in business
+          into a life of financial wisdom and entrepreneurship. You will learn from a 
+          successful CEO how to be profitable"
+        />
       </Grid>
 
       <Grid item sm={4} xs={12}>
-        <Typography className={classes.thirdRow}>Third Row</Typography>
+        <GridItem
+          title="Game Development Masterclass"
+          image="https://cdn.pixabay.com/photo/2020/12/31/21/03/deer-5877407_1280.jpg"
+          description="This bundle of 14 Unity cuourses will teach you how to build games 
+          from scratch. You will learn C#, Blender, Adobe Photoshop, Adobe Illutrator, and
+          Unity machine learning with Python"
+        />
       </Grid>
     </Grid>
   );
