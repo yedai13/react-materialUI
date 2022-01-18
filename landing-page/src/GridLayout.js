11e4c13b0a151e131532b79f59a5a4b5b1a07a5c
Facundo Marin <facundo.marin2012@gmail.com>
2022-01-18 10:21:14 -0300

    set up the grid layout

---
+++ landing-page/src/GridLayout.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+
+const GridLayout = () => {
+  return (
+    <Grid container>
+      <Grid item></Grid>
+
+      <Grid item></Grid>
+    </Grid>
+  );
+};
+
+export default GridLayout;
