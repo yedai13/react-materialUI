5d5ba24df55e31a8dcf25363e2bf2fefda78fcfc
Facundo Marin <facundo.marin2012@gmail.com>
2022-01-18 10:58:44 -0300

    style the main grid item

--- landing-page/src/MainGridItem.js
+++ landing-page/src/MainGridItem.js
@@ -4,31 +4,51 @@ import {
   Typography,
   Button,
   CardMedia,
+  Grid,
 } from "@material-ui/core";
-import { makeStyles } from "@material-ui/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import React from "react";
 
 const useStyles = makeStyles((theme) => ({
   cardMedia: {
     height: 300,
   },
+  mainItem: {
+    padding: theme.spacing(8),
+    textAlign: "left",
+  },
+  mainContainer: {
+    paddingTop: 120,
+    paddingBottom: 120,
+    background: "darkorange",
+  },
+  imageCard: {
+    margin: 40,
+  },
 }));
 const MainGridItem = () => {
-  const { cardMedia } = useStyles();
+  const { cardMedia, mainItem, mainContainer, imageCard } = useStyles();
   return (
-    <Container>
+    <Container className={mainContainer}>
       <Card>
-        <Typography>Unlimited Subcription</Typography>
-        <Typography>2,000 hours of online courses</Typography>
-        <Button color="primary" variant="contained">
-          Sign Up
-        </Button>
-        <Card>
-          <CardMedia
-            className={cardMedia}
-            image="https://cdn.pixabay.com/photo/2020/12/18/16/56/laptop-5842509_1280.jpg"
-          ></CardMedia>
-        </Card>
+        <Grid container>
+          <Grid item lg={6} md={6} sm={12} className={mainItem}>
+            <Typography variant="h2">Unlimited Subcription</Typography>
+            <Typography>2,000 hours of online courses</Typography>
+            <Button color="primary" variant="contained">
+              Sign Up
+            </Button>
+          </Grid>
+
+          <Grid item lg={6} md={6} sm={12}>
+            <Card className={imageCard}>
+              <CardMedia
+                className={cardMedia}
+                image="https://cdn.pixabay.com/photo/2020/12/18/16/56/laptop-5842509_1280.jpg"
+              ></CardMedia>
+            </Card>
+          </Grid>
+        </Grid>
       </Card>
     </Container>
   );
