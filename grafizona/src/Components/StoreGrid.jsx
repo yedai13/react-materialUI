e61ca47f683e95b121745019401243b65dcc5b64
Facundo Marin <facundo.marin2012@gmail.com>
2022-01-19 15:11:17 -0300

    fix justify items center in store grid

--- grafizona/src/Components/StoreGrid.jsx
+++ grafizona/src/Components/StoreGrid.jsx
@@ -1,4 +1,4 @@
-import { Grid } from "@material-ui/core";
+import { Container, Grid, Typography } from "@material-ui/core";
 import React from "react";
 import ItemStoreGrid from "./ItemStoreGrid";
 import akira from "../img/akira-remera.jpg";
@@ -9,79 +9,85 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   storeGrid: {
-    alignSelf: "center",
-    alignItems: "center",
+    justifyContent: "end",
   },
 }));
 
 const StoreGrid = () => {
   const { storeGrid } = useStyles();
   return (
-    <Grid container justifyContent="space-around" alignItems="center">
-      <Grid item lg={3} md={4} sm={6} xs={12}>
-        <ItemStoreGrid
-          title="AKIRA 2 - REMERA ESTILO ANIME"
-          image={akira}
-          price={2790}
-        />
-      </Grid>
+    <Container>
+      <Typography>TODO LO PUBLICADO SE ENCUENTRA EN STOCK.</Typography>
+      <Typography>
+        Si sos de zona Caba o GBA podes acceder a un 20% OFF extra abonando en
+        efectivo al recibir o retirando el producto por nuetro taller.
+      </Typography>
+      <Grid container>
+        <Grid item container justify="center" lg={3} md={4} sm={6} xs={12}>
+          <ItemStoreGrid
+            title="AKIRA 2 - REMERA ESTILO ANIME"
+            image={akira}
+            price={2790}
+          />
+        </Grid>
 
-      <Grid item lg={3} md={4} sm={6} xs={12}>
-        <ItemStoreGrid
-          title="REMERA COLOSSUS ESTILO SKN"
-          image={titancoloso}
-          price={2330}
-        />
-      </Grid>
+        <Grid item container justify="center" lg={3} md={4} sm={6} xs={12}>
+          <ItemStoreGrid
+            title="REMERA COLOSSUS ESTILO SKN"
+            image={titancoloso}
+            price={2330}
+          />
+        </Grid>
 
-      <Grid item lg={3} md={4} sm={6} xs={12}>
-        <ItemStoreGrid
-          title="REMERA BOARDING PASS"
-          image={boarding}
-          price={2590}
-        />
-      </Grid>
+        <Grid item container justify="center" lg={3} md={4} sm={6} xs={12}>
+          <ItemStoreGrid
+            title="REMERA BOARDING PASS"
+            image={boarding}
+            price={2590}
+          />
+        </Grid>
 
-      <Grid item lg={3} md={4} sm={6} xs={12}>
-        <ItemStoreGrid
-          title="REMERA ENCODING THE SKY"
-          image={encoding}
-          price={2790}
-        />
-      </Grid>
+        <Grid item container justify="center" lg={3} md={4} sm={6} xs={12}>
+          <ItemStoreGrid
+            title="REMERA ENCODING THE SKY"
+            image={encoding}
+            price={2790}
+          />
+        </Grid>
 
-      <Grid item lg={3} md={4} sm={6} xs={12}>
-        <ItemStoreGrid
-          title="AKIRA 2 - REMERA ESTILO ANIME"
-          image={akira}
-          price={2790}
-        />
-      </Grid>
+        <Grid item container justify="center" lg={3} md={4} sm={6} xs={12}>
+          <ItemStoreGrid
+            title="AKIRA 2 - REMERA ESTILO ANIME"
+            image={akira}
+            price={2790}
+          />
+        </Grid>
 
-      <Grid item lg={3} md={4} sm={6} xs={12}>
-        <ItemStoreGrid
-          title="REMERA COLOSSUS ESTILO SKN"
-          image={titancoloso}
-          price={2330}
-        />
-      </Grid>
+        <Grid item container justify="center" lg={3} md={4} sm={6} xs={12}>
+          <ItemStoreGrid
+            title="REMERA COLOSSUS ESTILO SKN"
+            image={titancoloso}
+            price={2330}
+          />
+        </Grid>
 
-      <Grid item lg={3} md={4} sm={6} xs={12}>
-        <ItemStoreGrid
-          title="REMERA BOARDING PASS"
-          image={boarding}
-          price={2590}
-        />
-      </Grid>
+        <Grid item container justify="center" lg={3} md={4} sm={6} xs={12}>
+          <ItemStoreGrid
+            title="REMERA BOARDING PASS"
+            image={boarding}
+            price={2590}
+          />
+        </Grid>
 
-      <Grid item lg={3} md={4} sm={6} xs={12}>
-        <ItemStoreGrid
-          title="REMERA ENCODING THE SKY"
-          image={encoding}
-          price={2790}
-        />
+        <Grid item container justify="center" lg={3} md={4} sm={6} xs={12}>
+          <ItemStoreGrid
+            title="REMERA ENCODING THE SKY"
+            image={encoding}
+            price={2790}
+          />
+        </Grid>
       </Grid>
-    </Grid>
+    </Container>
   );
 };
 
