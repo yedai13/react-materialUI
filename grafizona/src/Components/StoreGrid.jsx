b25bf660c9df52fc9d1f31f20fcf6c4b9c932a27
Facundo Marin <facundo.marin2012@gmail.com>
2022-01-19 11:43:54 -0300

    build store grid

---
+++ grafizona/src/Components/StoreGrid.jsx
@@ -0,0 +1,18 @@
+import { Grid } from "@material-ui/core";
+import React from "react";
+
+const StoreGrid = () => {
+  return (
+    <Grid container>
+      <Grid item></Grid>
+
+      <Grid item></Grid>
+
+      <Grid item></Grid>
+
+      <Grid item></Grid>
+    </Grid>
+  );
+};
+
+export default StoreGrid;
