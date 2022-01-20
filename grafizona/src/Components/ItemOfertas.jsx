0d78aa11e4d539e76beb051c83344ffaeddc902a
Facundo Marin <facundo.marin2012@gmail.com>
2022-01-20 12:52:01 -0300

    reuse item ofertas

--- grafizona/src/Components/ItemOfertas.jsx
+++ grafizona/src/Components/ItemOfertas.jsx
@@ -1,32 +1,34 @@
 import React from "react";
 import { Grid, Typography } from "@material-ui/core";
-import AttachMoneyIcon from "@material-ui/icons/AttachMoney";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
-  icon: {
-    fontSize: 40,
-  },
-  title: {
+  titleStyle: {
     fontWeight: "bold",
     fontSize: 18,
+    textAlign: "left",
+    [theme.breakpoints.down("md")]: {
+      textAlign: "center",
+    },
+  },
+  descriptionStyle: {
+    textAlign: "left",
+    [theme.breakpoints.down("md")]: {
+      textAlign: "center",
+    },
   },
 }));
 
-const ItemOfertas = () => {
-  const { icon, title } = useStyles();
+const ItemOfertas = ({ icon, title, description }) => {
+  const { titleStyle, descriptionStyle } = useStyles();
   return (
     <Grid container>
-      <Grid item lg={2}>
-        <AttachMoneyIcon className={icon} />
+      <Grid item lg={2} md={12} sm={12} xs={12}>
+        {icon}
       </Grid>
-      <Grid item lg={10}>
-        <Typography align="left" className={title}>
-          20% OFF PAGANDO EN EFECTIVO!
-        </Typography>
-        <Typography align="left">
-          Retirando por el taller o por MotoEnvio (Enviamos con prioridad)
-        </Typography>
+      <Grid item lg={10} md={12} sm={12} xs={12}>
+        <Typography className={titleStyle}>{title}</Typography>
+        <Typography className={descriptionStyle}>{description}</Typography>
       </Grid>
     </Grid>
   );
--- grafizona/src/Components/OfertasGrid.jsx
+++ grafizona/src/Components/OfertasGrid.jsx
@@ -1,18 +1,53 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
 import ItemOfertas from "./ItemOfertas";
+import AttachMoneyIcon from "@material-ui/icons/AttachMoney";
+import LocalShippingIcon from "@material-ui/icons/LocalShipping";
+import PaymentIcon from "@material-ui/icons/Payment";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  iconStyle: {
+    fontSize: 40,
+  },
+  gridLine: {
+    borderRight: "1px solid #000",
+    [theme.breakpoints.down(1000)]: {
+      border: 0,
+    },
+  },
+  gridContainer: {
+    marginTop: 30,
+    marginBottom: 30,
+  },
+}));
 
 const OfertasGrid = () => {
+  const { iconStyle, gridLine, gridContainer } = useStyles();
   return (
-    <Grid container>
-      <Grid item lg={4}>
-        <ItemOfertas />
+    <Grid container className={gridContainer}>
+      <Grid item lg={4} md={4} sm={12} xs={12} className={gridLine}>
+        <ItemOfertas
+          title=" 20% OFF PAGANDO EN EFECTIVO!"
+          description="Retirando por el taller o por MotoEnvio (Enviamos con prioridad)"
+          icon={<AttachMoneyIcon className={iconStyle} />}
+        />
       </Grid>
-      <Grid item lg={4}>
-        <ItemOfertas />
+
+      <Grid item lg={4} md={4} sm={12} xs={12} className={gridLine}>
+        <ItemOfertas
+          title="AHORA 3"
+          description="Compra paga en 3 coutas sin interes!"
+          icon={<PaymentIcon className={iconStyle} />}
+        />
       </Grid>
-      <Grid item lg={4}>
-        <ItemOfertas />
+
+      <Grid item lg={4} md={4} sm={12} xs={12}>
+        <ItemOfertas
+          title="ENVIOS A TODO EL PAIS!"
+          description="Aahora también enviamos por Correo Argentino"
+          icon={<LocalShippingIcon className={iconStyle} />}
+        />
       </Grid>
     </Grid>
   );
