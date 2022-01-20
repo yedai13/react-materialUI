f9484e6f08dbd4abcb2aa0c935a085f648a11ca6
Facundo Marin <facundo.marin2012@gmail.com>
2022-01-20 11:56:12 -0300

    build ofertas grid

---
+++ grafizona/src/Components/OfertasGrid.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+
+const OfertasGrid = () => {
+  return (
+    <Grid container>
+      <Grid item></Grid>
+      <Grid item></Grid>
+      <Grid item></Grid>
+    </Grid>
+  );
+};
+
+export default OfertasGrid;
